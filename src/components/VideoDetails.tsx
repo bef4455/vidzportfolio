0b30210c9a36a8e62b59d5b1f0d3a0ddc7a46a65
bef4 <befacool@gmail.com>
2024-11-09 14:38:41 +0100

    fix videodetails

--- src/components/VideoDetails.tsx
+++ src/components/VideoDetails.tsx
@@ -19,29 +19,44 @@ export default function VideoDetails() {
       )}
 
       {/* Contenu principal de la page */}
-      <div className="flex-grow flex flex-col justify-center items-center container mx-auto px-4 py-4 md:py-8 relative z-30">
+      <div className="flex-grow flex flex-col justify-center items-center container mx-auto px-2 py-4 relative z-30 md:py-8 md:px-4">
         {/* Titre et Détails de la Vidéo */}
-        <div className="mb-2 md:mb-4 text-center">
-          <h1 className="text-xl md:text-2xl font-bold mb-1">{title}</h1>
-          <p className="text-gray-500 text-sm md:text-base">{category}</p>
+        <div className="mb-4 md:mb-16 text-center">
+          <h1 className="text-lg md:text-3xl font-bold mb-2 md:mb-4">{title}</h1>
+          <p className="text-gray-500 text-sm md:text-base md:mb-8">{category}</p>
         </div>
 
         {/* Contenu de la Vidéo */}
         <div
-          className="w-full flex-grow flex items-center justify-center mb-8 cursor-pointer"
+          className="w-full flex items-center justify-center cursor-pointer md:flex-grow"
           onClick={handleVideoClick}
         >
           {videoUrl && (
-            <div className="w-full max-w-3xl h-[30vh] md:h-[40vh]">
-              <iframe
-                src={videoUrl}
-                title={title}
-                className="w-full h-full rounded-lg shadow-lg"
-                frameBorder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen
-              ></iframe>
-            </div>
+            <>
+              {/* Version Mobile */}
+              <div className="w-full max-w-3xl h-[50vh] md:hidden">
+                <iframe
+                  src={videoUrl}
+                  title={title}
+                  className="w-full h-full rounded-lg shadow-lg"
+                  frameBorder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowFullScreen
+                ></iframe>
+              </div>
+              
+              {/* Version Laptop */}
+              <div className="hidden md:block w-full h-[40vh] md:h-[60vh]">
+                <iframe
+                  src={videoUrl}
+                  title={title}
+                  className="w-full h-full rounded-lg shadow-lg"
+                  frameBorder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowFullScreen
+                ></iframe>
+              </div>
+            </>
           )}
         </div>
       </div>
